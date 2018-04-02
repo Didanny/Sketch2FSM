6c25b805d16b9587cd334c1c413dd8d96dcc6032
Didanny <37907412+Didanny@users.noreply.github.com>
2018-04-02 18:54:28 +0300

    Create ComponentWithStats class
    The simple container Components isn't enough, need member functions to get statistical data

---
+++ ComponentsWithStats.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "ComponentDetector.h"
+#include <cmath>
+
+typedef enum 
+{
+	CHAR,
+	CIRCLE,
+	ARROW
+} ComponentType;
+
+typedef enum
+{
+	BOX, // for the bounding_box stats
+	PIX // for the pixel stats
+} Stat;
+
+class ComponentsWithStats : public Components
+{
+private:
+	ComponentType m_type; // The type of the components int the 
+public:
+	ComponentsWithStats(); // Constructor
+	~ComponentsWithStats(); // Destructor
+	int getMeanArea(); // Returns the mean of the areas of the components
+	int getMeanBoxArea(); // Returns the mean of the areas of the bounding boxes of the components
+	double getVariance(Stat t_stat); // Returns variance of desired Stat
+	double getStdev(Stat t_stat); // Returns the standard deviation of the desired Stat
+};
