587838cff60bada87514f904bd1871c140326244
Didanny <37907412+Didanny@users.noreply.github.com>
2018-04-03 10:01:50 +0300

    Add m_container attribute to Component
    Will be used to identify the characters within the state circles

--- Component.cpp
+++ Component.cpp
@@ -2,7 +2,7 @@
 
 // Constructor
 Component::Component(int t_x, int t_y, int t_width, int t_height, int t_area, double t_cx, double t_cy)
-	: m_area(t_area)
+	: m_area(t_area), m_container(NULL)
 {
 	m_bounding_box = cv::Rect(t_x, t_y, t_width, t_height);
 	m_centroid = cv::Point(t_cx, t_cy);
@@ -22,7 +22,7 @@ int Component::getArea()
 	return m_area;
 }
 
-bool Component::contains(Component & t_component)
+bool Component::contains(Component& t_component)
 {
 	int my_left = getBoundingBox().x;
 	int my_right = my_left + getBoundingBox().width;
@@ -35,4 +35,14 @@ bool Component::contains(Component & t_component)
 	int their_bottom = their_top + t_component.getBoundingBox().height;
 
 	return (their_right < my_right && their_left > my_left && their_top > my_top && their_bottom < my_bottom);
+}
+
+void Component::setContainer(Component& t_component)
+{
+	m_container = &t_component;
+}
+
+Component * Component::getContainer()
+{
+	return m_container;
 }
--- Component.h
+++ Component.h
@@ -11,11 +11,14 @@ private:
 	cv::Rect m_bounding_box; // The bounding box of a component
 	int m_area; // The area of the component in pixels
 	cv::Point m_centroid; // The centroid of the components
-	bool is_enclosed; // true if component is entirely within another component
+	bool m_is_enclosed; // true if component is entirely within another component
+	Component* m_container; // The smallest component that encloses this component
 public:
 	Component(int t_x, int t_y, int t_width, int t_height, int t_area, double t_cx, double t_cy); // Constructor
 	~Component(); // Destructor
-	cv::Rect getBoundingBox();
+	cv::Rect getBoundingBox(); // Returns the bounding box of the component
 	int getArea(); // Returns the are in pixels
 	bool contains(Component& t_component); // Returns true if this bounding box is completely within the t_component bounding box
+	void setContainer(Component& t_component); // Sets m_container to t_component
+	Component* getContainer(); // Returns pointer to the container
 };
