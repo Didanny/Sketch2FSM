98093299dc4c950935e5572991eff5f3609412e0
Didanny <37907412+Didanny@users.noreply.github.com>
2018-04-16 18:00:04 +0300

    Fix initContainers

--- CircleClassifier.h
+++ CircleClassifier.h
@@ -2,7 +2,12 @@
 
 #include "Container.h"
 
-typedef std::vector<Container> Containers;
+//typedef std::vector<Container> Containers;
+class Containers : public std::vector<Container>
+{
+public:
+	int indexOf(Container t_container); // Returns index of t_container, -1 if not found
+};
 
 class CircleClassifier 
 {
@@ -12,9 +17,11 @@ private:
 public:
 	CircleClassifier(cv::Mat t_labeled_image); // Constructor
 	~CircleClassifier(); // Destructor
-	void initContainers(Components t_components); // initializes the containers
+	void initContainers(Components& t_components); // initializes the containers
+	void initChildren(Components& t_components); // initializes the children on m_containers
 	bool isCircle(Container t_container); // returns true if the container is in fact a circle
 	Containers getContainers(); // Returns containers
 	Containers findCircles(); // Returns the containers that are circles
 	void showCircles(); // Shows the circles in new window
+	void displayContainerLabels(); // Displays container labels
 };
