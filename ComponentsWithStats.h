bdd1d326ccf618721055248ac6e8effc56a4be06
Didanny <37907412+Didanny@users.noreply.github.com>
2018-04-22 19:45:48 +0300

    Add Heigth and Width metrics

--- ComponentsWithStats.h
+++ ComponentsWithStats.h
@@ -23,9 +23,17 @@ private:
 public:
 	ComponentsWithStats(); // Constructor
 	~ComponentsWithStats(); // Destructor
+	int indexOf(Component t_component); // Returns index of component if found, -1 if not
 	double getMeanArea(); // Returns the mean of the areas of the components
 	double getMeanBoxArea(); // Returns the mean of the areas of the bounding boxes of the components
 	double getVarianceArea(); // Returns variance of Pixel Areas
 	double getVarianceBoxArea(); // Returns variance of Box Areas
 	double getStdev(Stat t_stat); // Returns the standard deviation of the desired Stat
+	double getMeanHeight(); // Returns the mean of the heights of the components
+	double getMeanWidth(); // Returns the mean of the width
+	double getVarianceHeight(); // Returns the variance of the height
+	double getVarianceWidth(); // Returns the variance of the width
+	double getStdevHeight(); // Returns the standard deviation of the height
+	double getStdevWidth(); // Returns the standard deviation of the width
+	void findChars(Components& t_components);
 };
