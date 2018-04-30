19ead29e83398da454ff7897a41a712d9306c46e
Didanny <37907412+Didanny@users.noreply.github.com>
2018-05-01 00:57:36 +0300

    Define initArrows

--- ArrowClassifier.h
+++ ArrowClassifier.h
@@ -2,6 +2,7 @@
 
 #include "ComponentsWithStats.h"
 #include "ImageProcessor.h"
+#include "Arrow.h"
 
 class ArrowClassifier
 {
@@ -9,8 +10,11 @@ private:
 	//Components m_arrows;
 public:
 	Components m_arrows;
+	std::vector<Arrow> m_arrows_i;
+
 	ArrowClassifier(Components& t_components, std::vector<int> t_labels); // Constructor
 	~ArrowClassifier(); // Destructor
 	std::vector<int> getLabels();
 	cv::Point2f getNearestPoint(cv::Point2f t_point, std::vector<cv::Point2f> t_corners);
+	void initArrows(ImageProcessor& t_image_processor, ComponentDetector& t_component_detector); // Finds start and end of each arrow
 };
