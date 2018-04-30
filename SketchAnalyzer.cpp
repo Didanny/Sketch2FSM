07bfa2236e89cbdf3a6eb1bc4d54e8f909f4a52e
Didanny <37907412+Didanny@users.noreply.github.com>
2018-04-30 23:40:47 +0300

    Create SketchAnalyzer
    SketchAnalyzer will be the driver class that controls everything

---
+++ SketchAnalyzer.cpp
@@ -0,0 +1,50 @@
+#include "SketchAnalyzer.h"
+
+// Constructor
+SketchAnalyzer::SketchAnalyzer()
+{}
+
+// Loads image from given file path
+void SketchAnalyzer::loadImage(std::string t_image_path)
+{
+	m_image_processor = new ImageProcessor(t_image_path);
+}
+
+// Detects the connected components
+void SketchAnalyzer::findConnectedComponents()
+{
+	m_component_detector = new ComponentDetector(m_image_processor->forComponentDetector());
+	m_component_detector->detectComponents();
+}
+
+// Detect the containers, circle candidates
+void SketchAnalyzer::findContainers()
+{
+	m_component_detector->findContainers();
+	m_circle_classifier = new CircleClassifier(m_component_detector->getLabeledImage());
+	m_circle_classifier->initContainers(m_component_detector->getComponents());
+}
+
+// Finds the circles among the containers
+void SketchAnalyzer::findCircles()
+{
+	Containers circles = m_circle_classifier->findCircles(*m_image_processor, *m_component_detector);
+	m_circles = new Circles(circles);
+}
+
+// Finds the characters in and out of the circles
+void SketchAnalyzer::findCharacters()
+{
+	m_chars = m_circles->getChars();
+	m_chars.findChars(m_component_detector->getComponents());
+}
+
+// Finds the arrows
+void SketchAnalyzer::findArrows()
+{
+	std::vector<int> labels_all = m_circles->getLabels();
+	std::vector<int> labels = m_chars.getLabels();
+	labels_all.insert(labels_all.end(), labels.begin(), labels.end());
+
+	m_arrow_classifier = new ArrowClassifier(m_component_detector->getComponents(), labels_all);
+}
---
+++ SketchAnalyzer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "ImageProcessor.h"
+#include "CircleClassifier.h"
+#include "Circle.h"
+#include "ComponentsWithStats.h"
+#include "ArrowClassifier.h"
+
+class SketchAnalyzer
+{
+private:
+	//ImageProcessor* m_image_processor;
+public:
+	ImageProcessor * m_image_processor;
+	ComponentDetector* m_component_detector;
+	CircleClassifier* m_circle_classifier;
+	Circles* m_circles;
+	ArrowClassifier* m_arrow_classifier;
+
+	ComponentsWithStats m_chars;
+	Components m_components; // Vector of the connected components
+
+	SketchAnalyzer(); // Constructor
+	void loadImage(std::string t_image_path); // Loads image from given path
+	void findConnectedComponents(); // Detects connected components
+	void findContainers(); // Finds the containers, circle candidates
+	void findCircles(); // Finds the circles among the containers
+	void findCharacters(); // Finds the characters in and out of the circles
+	void findArrows(); // Finds the arrows
+};
