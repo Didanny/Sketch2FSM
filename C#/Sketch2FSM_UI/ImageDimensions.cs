3b9d6763eb18e0fe132c2eb232e0c361b2ca77ce
Didanny <37907412+Didanny@users.noreply.github.com>
2018-05-06 20:39:02 +0300

    Create static ImageDimensions class

---
+++ C#/Sketch2FSM_UI/ImageDimensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sketch2FSM_UI
+{
+    public static class ImageDimensions
+    {
+        public static class Image
+        {
+            public static int width;
+            public static int height;
+        }
+
+        public static class Panel
+        {
+            public static int width;
+            public static int height;
+        }
+    }
+}
