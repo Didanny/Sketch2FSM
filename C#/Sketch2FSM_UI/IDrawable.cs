35d642654b7cc90ad1724c59086596be49e20f84
Didanny <37907412+Didanny@users.noreply.github.com>
2018-05-06 20:38:05 +0300

    Create IDrawable interface

---
+++ C#/Sketch2FSM_UI/IDrawable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Sketch2FSM_UI
+{
+    public interface IDrawable
+    {
+        //void Draw(Point location);
+        void Draw(Canvas canvas, SolidColorBrush color);
+        bool Equals(IDrawable drawable);
+    }
+}
