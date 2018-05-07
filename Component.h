d782c38ae80ed4d9802c2775e1be0c929cf73003
Didanny <37907412+Didanny@users.noreply.github.com>
2018-05-07 09:16:59 +0300

    Change Component to struct
    Was inconsistently using public members and getters, so made everything public

--- Component.h
+++ Component.h
@@ -5,35 +5,29 @@
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 
-class Component
+//#include "C#\sketch2fsmlib\stdafx.h"
+
+
+struct Component
 {
-private:
+
 	cv::Rect m_bounding_box; // The bounding box of a component
 	int m_area; // The area of the component in pixels
 	cv::Point m_centroid; // The centroid of the components
-	bool m_is_enclosed; // true if component is entirely within another component
 	Component* m_container; // The smallest component that encloses this component
-	bool m_is_container; // true if component is container
-	bool m_is_classified; // true if the component has been classified
-public:
-	int m_label;
+	int m_label; // The label given to component by cv::connectedComponents
+	bool m_is_container; // True if component is container;
+
 	Component(int t_x, int t_y, int t_width, int t_height, int t_area, double t_cx, double t_cy, int t_label); // Constructor
 	~Component(); // Destructor
-	cv::Rect getBoundingBox(); // Returns the bounding box of the component
-	int getArea(); // Returns the are in pixels
+	//cv::Rect m_bounding_box; // Returns the bounding box of the component
+	//int m_area; // Returns the are in pixels
 	bool contains(Component& t_component); // Returns true if this bounding box is completely within the t_component bounding box
 	void setContainer(Component& t_component); // Sets m_container to t_component
 	Component* getContainer(); // Returns pointer to the container
 	cv::Point getCentroid(); // Returns the centroid
 	Component copy(); // Returns a copy of the component
-	int getLabel(); // Returns label
-	bool equals(Component t_component); // Returns true if equal
-	bool operator<(Component& t_right); // Returns true if label less than
-	void setAsContainer(); // Sets the surrent component as a container
-	bool isContainer(); // Returns true if component is container
-	bool isClassified(); // Returns true if component has already been classified
-	void setClassified(); // Sets the components as classified
+	bool isContainer();
+	void setAsContainer();
+	//int m_label; // Returns label
 };
-
-bool operator== (Component t_lhs, Component t_right); // Returns true if equal
-bool operator< (Component t_lhs, Component t_right); // Returns true if label less than, no logical purpose just to enable sorting
